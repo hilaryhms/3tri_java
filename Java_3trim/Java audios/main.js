d13289bcb732cff6a99a58c0f4ab38fe7222bd7d
hilaryhms <105428114+hilaryhms@users.noreply.github.com>
2023-11-08 11:40:11 -0300

    Update main.js

--- Java_3trim/Java audios/main.js
+++ Java_3trim/Java audios/main.js
@@ -1,1 +1,17 @@
-
+function tocaSom(idElemetoAudio){
+    document.querySelector(idElemetoAudio).play();
+}
+const listadeteclas=document.querySelectorA11('.tecla');
+let contador=0;
+while(contador <listadeteclas.length){
+    const efeito= listadeteclas[contador].classList[1];
+    const idAudio="#som_"+efeito;
+    listadeteclas[contador].onclick=function(){
+        tocaSom(idAudio);
+    }
+    contador=contador +1 ;
+    console.log(contador);
+}
+WebTransportBidirectionalStream.onkeydown=function(){
+    tecla.add('ativa')
+}
